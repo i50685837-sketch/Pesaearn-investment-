c6d96907dffe0b9b170f4ba215fa696b07b7744b
i50685837-sketch <i50685837@gmail.com>
2026-07-25 10:01:02 +0300

    Create pesapal.js

---
+++ config/pesapal.js
@@ -0,0 +1,13 @@
+module.exports={
+
+baseUrl:process.env.PESAPAL_BASE_URL,
+
+consumerKey:process.env.PESAPAL_CONSUMER_KEY,
+
+consumerSecret:process.env.PESAPAL_CONSUMER_SECRET,
+
+callbackUrl:process.env.PESAPAL_CALLBACK_URL,
+
+notificationId:process.env.PESAPAL_NOTIFICATION_ID
+
+};

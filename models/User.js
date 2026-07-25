a8546d86f185a59ad69da956f32a14b551921981
i50685837-sketch <i50685837@gmail.com>
2026-07-25 09:51:27 +0300

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    phone: {
+      type: String
+    },
+
+    walletBalance: {
+      type: Number,
+      default: 0
+    },
+
+    investmentBalance: {
+      type: Number,
+      default: 0
+    },
+
+    rewardPoints: {
+      type: Number,
+      default: 0
+    },
+
+    profileImage: {
+      type: String,
+      default: ""
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
